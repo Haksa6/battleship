56f53e1c6de5cebf1c4c4381fcc5a5575fc1e875
Haksa6 <77583826+Haksa6@users.noreply.github.com>
2022-07-07 09:52:44 +0300

    ships on board actually

--- src/components/Ship.jsx
+++ src/components/Ship.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 const Ship = ({ ship }) => {
   const { row, col } = ship.getStartCoords();
 
+  //Get the correct size and position even with border
   const shipStyle = {
     top: row * 32,
     left: col * 32,
-    width: ship.getDirection() ? 30 * ship.getSize() + 1 : '28px',
-    height: ship.getDirection() ? '28px' : 30 * ship.getSize() + 1,
+    width: ship.getDirection()
+      ? 28 * ship.getSize() + (ship.getSize() - 1) * 4
+      : '28px',
+    height: ship.getDirection()
+      ? '28px'
+      : 28 * ship.getSize() + (ship.getSize() - 1) * 4,
   };
 
   return (
